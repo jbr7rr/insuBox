1379fd6378f95395227df4a89c4fd9f26b2c2307
jbr7rr <jbr7rr@users.noreply.github.com>
2024-05-18 18:55:32 +0200

    WriteCommandPackets

---
+++ lib/pump/medtrum/utils/CrcUtil.h
@@ -0,0 +1,17 @@
+#ifndef CRCUTIL_H
+#define CRCUTIL_H
+
+
+#include <cstdint>
+#include <cstring>
+
+
+class CrcUtil
+{
+public:
+    static uint8_t crc8(const uint8_t *value, size_t size, uint8_t crc8 = 0);
+
+private:
+};
+
+#endif // CRCUTIL_H
