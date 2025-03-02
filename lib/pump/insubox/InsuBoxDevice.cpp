a6553ba55a43887cdaf8386f76ed864e8cb15fd9
jbr7rr <jbr7rr@users.noreply.github.com>
2025-03-02 14:53:13 +0100

    insubox_rev_zero: buzzer en stepper

--- lib/pump/insubox/InsuBoxDevice.cpp
+++ lib/pump/insubox/InsuBoxDevice.cpp
@@ -3,80 +3,39 @@
 #define LOG_LEVEL LOG_LEVEL_DBG
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(ib_virtual_pump_device);
+LOG_MODULE_REGISTER(ib_insubox_pump_device);
 
 // TODO: move low level stuff to its own files
 #include <cmath>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/kernel.h>
-static const struct pwm_dt_spec pwm_step_vref = PWM_DT_SPEC_GET(DT_ALIAS(pwm_step_vref));
 
 // Stepper device
 #include <zephyr/drivers/stepper.h>
+static const struct pwm_dt_spec pwm_step_vref = PWM_DT_SPEC_GET(DT_ALIAS(pwm_step_vref));
 static const struct device *stepper_dev = DEVICE_DT_GET(DT_NODELABEL(motor_1));
 
-// Sensor inputs
-#define PWN_SENSOR_NODE DT_INST(0, test_pwm_loopback)
-
-#define PWM_BUZZER_OUT_CTRL DT_PWMS_CTLR_BY_IDX(PWN_SENSOR_NODE, 0)
-#define PWM_BUZZER_OUT_CHANNEL DT_PWMS_CHANNEL_BY_IDX(PWN_SENSOR_NODE, 0)
-#define PWM_BUZZER_OUT_FLAGS DT_PWMS_FLAGS_BY_IDX(PWN_SENSOR_NODE, 0)
-
-#define PWM_SENSOR_0_IN_CTRL DT_PWMS_CTLR_BY_IDX(PWN_SENSOR_NODE, 1)
-#define PWM_SENSOR_0_IN_CHANNEL DT_PWMS_CHANNEL_BY_IDX(PWN_SENSOR_NODE, 1)
-#define PWM_SENSOR_0_IN_FLAGS DT_PWMS_FLAGS_BY_IDX(PWN_SENSOR_NODE, 1)
-
-#define PWM_SENSOR_1_IN_CTRL DT_PWMS_CTLR_BY_IDX(PWN_SENSOR_NODE, 2)
-#define PWM_SENSOR_1_IN_CHANNEL DT_PWMS_CHANNEL_BY_IDX(PWN_SENSOR_NODE, 2)
-#define PWM_SENSOR_1_IN_FLAGS DT_PWMS_FLAGS_BY_IDX(PWN_SENSOR_NODE, 2)
-
-// TODO: Move to header or whatever
-struct pwm_sensor
-{
-    const struct device *dev;
-    uint32_t pwm;
-    pwm_flags_t flags;
-};
-
-struct pwm_callback_data
-{
-    uint32_t *buffer;
-    size_t buffer_len;
-    size_t count;
-    int status;
-    struct k_sem sem;
-    bool pulse_capture;
-};
-
-struct pwm_sensor pwm_buzzer = {
-    .dev = DEVICE_DT_GET(PWM_BUZZER_OUT_CTRL),
-    .pwm = PWM_BUZZER_OUT_CHANNEL,
-    .flags = PWM_BUZZER_OUT_FLAGS,
-};
-
-struct pwm_sensor pwm_sensor_0 = {
-    .dev = DEVICE_DT_GET(PWM_SENSOR_0_IN_CTRL),
-    .pwm = PWM_SENSOR_0_IN_CHANNEL,
-    .flags = PWM_SENSOR_0_IN_FLAGS,
-};
-
-struct pwm_sensor pwm_sensor_1 = {
-    .dev = DEVICE_DT_GET(PWM_SENSOR_1_IN_CTRL),
-    .pwm = PWM_SENSOR_1_IN_CHANNEL,
-    .flags = PWM_SENSOR_1_IN_FLAGS,
-};
+// Buzzer device
+static const struct pwm_dt_spec pwm_buzzer = PWM_DT_SPEC_GET(DT_ALIAS(pwm_buzzer));
 
 // Define note frequencies (in Hz)
 enum notes
 {
-    NOTE_C4 = 261,
-    NOTE_D4 = 294,
-    NOTE_E4 = 329,
-    NOTE_F4 = 349,
-    NOTE_G4 = 392,
-    NOTE_A4 = 440,
-    NOTE_B4 = 494,
-    NOTE_C5 = 523,
+    NOTE_C6 = 1047,
+    NOTE_D6 = 1175,
+    NOTE_E6 = 1319,
+    NOTE_F6 = 1397,
+    NOTE_G6 = 1568,
+    NOTE_A6 = 1760,
+    NOTE_B6 = 1976,
+    NOTE_C7 = 2093,
+    NOTE_D7 = 2349,
+    NOTE_E7 = 2637,
+    NOTE_F7 = 2794,
+    NOTE_G7 = 3136,
+    NOTE_A7 = 3520,
+    NOTE_B7 = 3951,
+    NOTE_C8 = 4186,
 };
 
 // Example tune: {note, duration_in_ms}
@@ -85,8 +44,8 @@ const struct
     int frequency;
     int duration;
 } tune[] = {
-    {NOTE_C4, 1000}, {NOTE_D4, 1000}, {NOTE_E4, 1000}, {NOTE_F4, 1000},
-    {NOTE_G4, 1000}, {NOTE_A4, 1000}, {NOTE_B4, 1000}, {NOTE_C5, 1000},
+    {NOTE_C6, 500}, {NOTE_E6, 500}, {NOTE_G6, 500}, {NOTE_B6, 500},
+    {NOTE_D7, 500}, {NOTE_F7, 500}, {NOTE_A7, 500}, {NOTE_C8, 500},
 };
 
 static void drv_callback(const struct device *dev, enum stepper_event event, void *dummy)
@@ -161,7 +120,6 @@ void InsuBoxDevice::init()
 {
     LOG_DBG("InsuBoxDevice init");
 
-    int ret = 0;
 
     k_sleep(K_MSEC(1000));
 
@@ -171,82 +129,49 @@ void InsuBoxDevice::init()
         return;
     }
 
-    if (!device_is_ready(pwm_buzzer.dev))
+    if (!pwm_is_ready_dt(&pwm_buzzer))
     {
-        LOG_ERR("Error: PWM buzzer device is not ready\n");
-        return;
-    }
-
-    if (!device_is_ready(pwm_sensor_0.dev))
-    {
-        LOG_ERR("Error: PWM sensor 0 device is not ready\n");
-        return;
-    }
-
-    if (!device_is_ready(pwm_sensor_1.dev))
-    {
-        LOG_ERR("Error: PWM sensor 1 device is not ready\n");
+        LOG_ERR("Error: PWM device %s is not ready\n", pwm_buzzer.dev->name);
         return;
     }
 
     stepper_set_event_callback(stepper_dev, drv_callback, NULL);
 
-    // TEST BUZZER
-    int period = 0;
-    int pulse = 0;
+    // // TEST BUZZER
+    // int ret = 0;
+    // int period = 5000;
+    // int pulse = 0;
 
-    for (size_t i = 0; i < ARRAY_SIZE(tune); i++)
-    {
-        period = 10000000000 / tune[i].frequency;
-        pulse = period / 2;
-        ret = pwm_set(pwm_buzzer.dev, pwm_buzzer.pwm, period, pulse, pwm_buzzer.flags);
-        if (ret)
-        {
-            LOG_ERR("Error %d: failed to set pulse width\n", ret);
-            return;
-        }
+    // for (size_t i = 0; i < ARRAY_SIZE(tune); i++)
+    // {
+    //     period = 1000000000 / tune[i].frequency;
+    //     pulse = period / 2;
+    //     LOG_DBG("Playing note %d, period %d, pulse %d", tune[i].frequency, period, pulse);
+    //     ret = pwm_set_dt(&pwm_buzzer, period, pulse);
+    //     if (ret)
+    //     {
+    //         LOG_ERR("Error %d: failed to set pulse width\n", ret);
+    //         return;
+    //     }
 
-        k_sleep(K_MSEC(tune[i].duration));
-    }
+    //     k_sleep(K_MSEC(tune[i].duration));
+    // }
 
-    pwm_set(pwm_buzzer.dev, pwm_buzzer.pwm, period, 0, pwm_buzzer.flags);
+    // pwm_set_dt(&pwm_buzzer, period, 0);
 
-    k_work_reschedule(&mSubContainer.sensorWork, K_NO_WAIT);
+    // k_work_reschedule(&mSubContainer.sensorWork, K_NO_WAIT);
 
     // movePlunger(10, false);
     // k_sleep(K_SECONDS(30));
     // movePlunger(-350, true);
     // k_sleep(K_SECONDS(90));
-    movePlunger(300, true);
+    movePlunger(300, false);
     // k_sleep(K_SECONDS(30));
     // movePlunger(10, false);
 }
 
 void InsuBoxDevice::sensorWork()
 {
-    // Test PWM sensor
-    uint64_t period_capture_0 = 0;
-    uint64_t pulse_capture_0 = 0;
-    uint64_t period_capture_1 = 0;
-    uint64_t pulse_capture_1 = 0;
-    int ret = 0;
-
-    ret = pwm_capture_nsec(pwm_sensor_0.dev, pwm_sensor_0.pwm, (PWM_CAPTURE_TYPE_BOTH | pwm_sensor_0.flags),
-                           &period_capture_0, &pulse_capture_0, K_MSEC(10));
-    if (ret)
-    {
-        LOG_ERR("Error %d: failed to capture PWM sensor 0\n", ret);
-    }
-
-    ret = pwm_capture_nsec(pwm_sensor_1.dev, pwm_sensor_1.pwm, (PWM_CAPTURE_TYPE_BOTH | pwm_sensor_0.flags),
-                           &period_capture_1, &pulse_capture_1, K_MSEC(10));
-    if (ret)
-    {
-        LOG_ERR("Error %d: failed to capture PWM sensor 1\n", ret);
-    }
-
-    double duty_cycle_0 = (float)pulse_capture_0 / (float)period_capture_0;
-    double duty_cycle_1 = (float)pulse_capture_1 / (float)period_capture_1;
-    LOG_DBG("PWM sensor 0: %f, PWM sensor 1: %f", duty_cycle_0, duty_cycle_1);
+    LOG_DBG("sensorWork");
     k_work_reschedule(&mSubContainer.sensorWork, K_MSEC(500));
 }
